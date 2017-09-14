22290903f8f8a8aa1ceda1a99f97fe432fdb9d01
baob <coder@onesandthrees.com>
2017-09-14 12:21:34 +0100

    marianos tweaks to make test class more flexible

---
+++ test/PolicyRandomTest.kt
@@ -0,0 +1,15 @@
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class PolicyRandomTest {
+
+    @Test
+    fun `responds to play() with a board`() {
+        val board = Board()
+        val player = 1
+        val playRandom = PolicyRandom().play(board, player)
+
+        assert(playRandom is Board)
+    }
+
+}
