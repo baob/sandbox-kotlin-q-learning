27a2f04f544eafd004c1bcafa3048c031a377b9d
baob <coder@onesandthrees.com>
2017-09-27 18:27:44 +0100

    add tests for yet another move

--- test/BoardTest.kt
+++ test/BoardTest.kt
@@ -97,3 +97,36 @@ class `board after player 1 moves to 8 and player 2 moves to 7` {
         assertEquals("...\n...\n.OX\n", board.toString("XO"))
     }
 }
+
+class `board after player 1 moves to 8 and player 2 moves to 7 and player 1 moves to 0` {
+
+    val board = Board().applyMove(8,1).applyMove(7,2).applyMove(0,1)
+    val moves = board.moveOptions()
+    val expectedBoardMoves = listOf(1, 2, 3, 4, 5, 6)
+    val expectedBoardList = listOf(1, 0, 0, 0, 0, 0, 0, 2, 1)
+
+    @Test
+    fun `responds to moveOptions with 0 to 6`() {
+        assertEquals(expectedBoardMoves, moves)
+    }
+
+    @Test
+    fun `responds to toState with 6568`() {
+        assertEquals( (1*9*9*9*9 + 2*3 + 1), board.toState())
+    }
+
+    @Test
+    fun `responds to toArray with arrayOf(zeroes) except 8 is 1 and 7 is 2`() {
+        assertThat(board.toList(), equalTo(expectedBoardList))
+    }
+
+    @Test
+    fun `responds to toString with 1 in position 8 and 2 in 7`() {
+        assertEquals("1..\n...\n.21\n", board.toString(null))
+    }
+
+    @Test
+    fun `responds to toString XO with X in position 8 and O in 7`() {
+        assertEquals("X..\n...\n.OX\n", board.toString("XO"))
+    }
+}
