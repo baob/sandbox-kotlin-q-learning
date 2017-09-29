55a82a435bdc05c877a542bf2bf280a9c2936205
baob <coder@onesandthrees.com>
2017-09-29 15:43:45 +0100

    add MutableList<e>#pop

--- src/MutableList.kt
+++ src/MutableList.kt
@@ -1,5 +1,10 @@
 
+
 fun <E> MutableList<E>.push(e: E): MutableList<E> {
     this.add(this.size , e)
     return this
+}
+
+fun <E> MutableList<E>.pop(): E {
+    return removeAt(size - 1)
 }
--- test/MutableListTest.kt
+++ test/MutableListTest.kt
@@ -4,15 +4,31 @@ import org.junit.Test
 
 //var s = mutableListOf(1,2,3)
 //var s2 = s.add(1,2)
-//var s3 = s.push(88)
+//var s3 = s.removeAt()     removeAt()
 
 class `a mutable list with elements 1 2 3` {
-    var list: MutableList<Int> = mutableListOf(1,2,3)
+    var list1: MutableList<Int> = mutableListOf(1,2,3)
 
     @Test
     fun `push(21) should give 1 2 3 21`() {
-        assertThat(list.push(21), equalTo(mutableListOf(1,2,3,21)))
+        assertThat(list1.push(21), equalTo(mutableListOf(1,2,3,21)))
 
+    }
+}
+
+class `a mutable list with elements 1 2 4` {
+
+    @Test
+    fun `pop() should change the list to 1 2`() {
+        var list2 = mutableListOf(1,2,4)
+        list2.pop()
+        assertThat(list2, equalTo(mutableListOf(1,2)))
+    }
+
+    @Test
+    fun `pop() should return 4`() {
+        var list3 = mutableListOf(1,2,4)
+        assertThat(list3.pop(), equalTo(4))
     }
 }
 
